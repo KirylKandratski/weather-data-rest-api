2cfd2f1656d7ece3092ed28226e151437f14fca8
KirylKandratski <kiryl.kandratski@gmail.com>
2023-04-11 09:27:09 +0200

    Initial Commit

---
+++ src/main/java/kandratski/petprojects/weatherdatarestapi/WeatherDataRestApiApplication.java
@@ -0,0 +1,13 @@
+package kandratski.petprojects.weatherdatarestapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeatherDataRestApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WeatherDataRestApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/kandratski/petprojects/weatherdatarestapi/WeatherDataRestApiApplicationTests.java
@@ -0,0 +1,13 @@
+package kandratski.petprojects.weatherdatarestapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WeatherDataRestApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
