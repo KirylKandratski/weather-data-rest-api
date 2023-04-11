c03e7dfa9d446436566f7fb7a3525a9767d5c714
KirylKandratski <kiryl.kandratski@gmail.com>
2023-04-11 11:01:09 +0200

    feat: add entities Measurement, Sensor.

---
+++ src/main/java/kandratski/petprojects/weatherdatarestapi/entity/Measurement.java
@@ -0,0 +1,97 @@
+package kandratski.petprojects.weatherdatarestapi.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotNull;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "measurements")
+public class Measurement {
+
+    @Id
+    @Column(name = "measurement_id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer measurement_id;
+
+    @Column(name = "value", nullable = false)
+    @NotNull(message = "Value should not be null")
+    @Min(value = -100, message = "Value must be greater than -100")
+    @Max(value = 100, message = "Value must be less than 100")
+    private Float value;
+
+    @Column(name = "raining", nullable = false)
+    @NotNull(message = "Raining should not be null")
+    private Boolean raining;
+
+    @Column(name = "recording_time", nullable = false)
+    private LocalDateTime recordingTime;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "sensor_id", nullable = false)
+    private Sensor sensor;
+
+    public Measurement() {
+    }
+
+    public Integer getMeasurement_id() {
+        return measurement_id;
+    }
+
+    public void setMeasurement_id(Integer measurement_id) {
+        this.measurement_id = measurement_id;
+    }
+
+    public Float getValue() {
+        return value;
+    }
+
+    public void setValue(Float value) {
+        this.value = value;
+    }
+
+    public Boolean getRaining() {
+        return raining;
+    }
+
+    public void setRaining(Boolean raining) {
+        this.raining = raining;
+    }
+
+    public LocalDateTime getRecordingTime() {
+        return recordingTime;
+    }
+
+    public void setRecordingTime(LocalDateTime recordingTime) {
+        this.recordingTime = recordingTime;
+    }
+
+    public Sensor getSensor() {
+        return sensor;
+    }
+
+    public void setSensor(Sensor sensor) {
+        this.sensor = sensor;
+    }
+
+    @Override
+    public String toString() {
+        return "Measurement{" +
+               "measurement_id=" + measurement_id +
+               ", value=" + value +
+               ", raining=" + raining +
+               ", recordingTime=" + recordingTime +
+               ", sensor=" + sensor +
+               '}';
+    }
+}
---
+++ src/main/java/kandratski/petprojects/weatherdatarestapi/entity/Sensor.java
@@ -0,0 +1,70 @@
+package kandratski.petprojects.weatherdatarestapi.entity;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+
+import java.util.List;
+
+@Entity
+@Table(name = "sensors")
+public class Sensor {
+
+    @Id
+    @Column(name = "sensor_id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer sensor_id;
+
+    @Column(name = "name", nullable = false)
+    @NotNull(message = "Name should not be empty")
+    @Size(min = 3, max = 30, message = "Name should be between 3 and 30 characters")
+    private String name;
+
+    @OneToMany(mappedBy = "sensor", cascade = CascadeType.ALL, fetch = FetchType.LAZY)
+    private List<Measurement> measurements;
+
+
+    public Sensor() {
+    }
+
+    public Integer getSensor_id() {
+        return sensor_id;
+    }
+
+    public void setSensor_id(Integer sensor_id) {
+        this.sensor_id = sensor_id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public List<Measurement> getMeasurements() {
+        return measurements;
+    }
+
+    public void setMeasurements(List<Measurement> measurements) {
+        this.measurements = measurements;
+    }
+
+    @Override
+    public String toString() {
+        return "Sensor{" +
+               "sensor_id=" + sensor_id +
+               ", name='" + name + '\'' +
+               ", measurements=" + measurements +
+               '}';
+    }
+}
